d500002857e7e569326206d9c0971999dabc74fb
ankit21a <ankit21.a@tcs.com>
2014-05-14 18:11:58 +0530

    Incorporated review comments

--- neutron/src/main/java/org/opendaylight/opencontrail/neutron/Activator.java
+++ neutron/src/main/java/org/opendaylight/opencontrail/neutron/Activator.java
@@ -6,7 +6,6 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  *
  */
-
 package org.opendaylight.opencontrail.neutron;
 
 import net.juniper.contrail.api.ApiConnector;
@@ -20,40 +19,41 @@ import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
 import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 /**
  * OSGi bundle activator for the opencontrail Neutron Interface.
  */
-
 public class Activator extends ComponentActivatorAbstractBase {
-    static ApiConnector apiConnector=null;
+    static ApiConnector apiConnector = null;
     static final Logger LOGGER = LoggerFactory.getLogger(Activator.class);
+
     /**
-     * Function called when the activator starts just after some
-     * initializations are done by the
-     * ComponentActivatorAbstractBase.
+     * Function called when the activator starts just after some initializations
+     * are done by the ComponentActivatorAbstractBase.
      */
     @Override
     public void init() {
         LOGGER.info("opencontrail Plugin service Registered");
-        apiConnector =getApiConnection();
+        apiConnector = getApiConnection();
     }
 
     /**
-     * Function called to get APIConnector object.
-     * porperties must be defined in opendaylight configuration folder inside config.ini
-     * at opendaylight/distribution/opendaylight/src/main/resources/configuration/config.ini
+     * Function called to get APIConnector object. porperties must be defined in
+     * opendaylight configuration folder inside config.ini at
+     * opendaylight/distribution
+     * /opendaylight/src/main/resources/configuration/config.ini
      */
-    public ApiConnector getApiConnection(){
-        String ipAddress=System.getProperty("opencontrail.apiserver.ipaddress");
-        String port=System.getProperty("opencontrail.apiserver.port");
-        int portNumber=Integer.parseInt(port);
+    public ApiConnector getApiConnection() {
+        String ipAddress = System.getProperty("opencontrail.apiserver.ipaddress");
+        String port = System.getProperty("opencontrail.apiserver.port");
+        int portNumber = Integer.parseInt(port);
         apiConnector = ApiConnectorFactory.build(ipAddress, portNumber);
         return apiConnector;
     }
 
     /**
-     * Function called when the activator stops just before the
-     * cleanup done by ComponentActivatorAbstractBase.
+     * Function called when the activator stops just before the cleanup done by
+     * ComponentActivatorAbstractBase.
      *
      */
     @Override
@@ -61,52 +61,47 @@ public class Activator extends ComponentActivatorAbstractBase {
     }
 
     /**
-     * Function that is used to communicate to dependency manager the
-     * list of known implementations for services inside a container.
+     * Function that is used to communicate to dependency manager the list of
+     * known implementations for services inside a container.
      *
      * @return An array containing all the CLASS objects that will be
-     * instantiated in order to get an fully working implementation
-     * Object
+     *         instantiated in order to get an fully working implementation
+     *         Object
      */
     @Override
     public Object[] getImplementations() {
-        Object[] res = {NetworkHandler.class,SubnetHandler.class,PortHandler.class};
+        Object[] res = { NetworkHandler.class, SubnetHandler.class, PortHandler.class };
         return res;
     }
 
     /**
-     * Function that is called when configuration of the dependencies
-     * is required.
+     * Function that is called when configuration of the dependencies is
+     * required.
      *
-     * @param c dependency manager Component object, used for
-     * configuring the dependencies exported and imported
-     * @param imp Implementation class that is being configured,
-     * needed as long as the same routine can configure multiple
-     * implementations
-     * @param containerName The containerName being configured, this allow
-     * also optional per-container different behavior if needed, usually
-     * should not be the case though.
+     * @param c
+     *            dependency manager Component object, used for configuring the
+     *            dependencies exported and imported
+     * @param imp
+     *            Implementation class that is being configured, needed as long
+     *            as the same routine can configure multiple implementations
+     * @param containerName
+     *            The containerName being configured, this allow also optional
+     *            per-container different behavior if needed, usually should not
+     *            be the case though.
      */
     @Override
-    public void configureInstance(Component c, Object imp,
-                                  String containerName) {
+    public void configureInstance(Component c, Object imp, String containerName) {
         if (imp.equals(NetworkHandler.class)) {
             c.setInterface(INeutronNetworkAware.class.getName(), null);
         }
-
-      if (imp.equals(SubnetHandler.class)) {
+        if (imp.equals(SubnetHandler.class)) {
             c.setInterface(INeutronSubnetAware.class.getName(), null);
         }
-
-      if (imp.equals(PortHandler.class)) {
-          c.setInterface(INeutronPortAware.class.getName(), null);
-      }
-
+        if (imp.equals(PortHandler.class)) {
+            c.setInterface(INeutronPortAware.class.getName(), null);
+        }
         // Create service dependencies.
-        c.add(createServiceDependency()
-                .setService(BindingAwareBroker.class)
-                .setCallbacks("setBindingAwareBroker", "unsetBindingAwareBroker")
+        c.add(createServiceDependency().setService(BindingAwareBroker.class).setCallbacks("setBindingAwareBroker", "unsetBindingAwareBroker")
                 .setRequired(true));
-
     }
 }
--- neutron/src/test/java/org/opendaylight/opencontrail/neutron/SubnetHandlerTest.java
+++ neutron/src/test/java/org/opendaylight/opencontrail/neutron/SubnetHandlerTest.java
@@ -6,7 +6,6 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  *
  */
-
 package org.opendaylight.opencontrail.neutron;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -33,26 +32,25 @@ import org.opendaylight.controller.networkconfig.neutron.NeutronSubnet;
  * Test Class for Subnet.
  */
 public class SubnetHandlerTest {
-
     SubnetHandler subnetHandler;
     VirtualNetwork mockedVirtualNetwork = mock(VirtualNetwork.class);
     ApiConnector mockedApiConnector = mock(ApiConnector.class);
 
     @Before
-    public void beforeTest(){
+    public void beforeTest() {
         subnetHandler = new SubnetHandler();
         assertNotNull(mockedApiConnector);
         assertNotNull(mockedVirtualNetwork);
     }
 
     @After
-    public void afterTest(){
+    public void afterTest() {
         subnetHandler = null;
         Activator.apiConnector = null;
     }
 
-    /* dummy params for Neutron Subnet  */
-    public NeutronSubnet defaultSubnetObject(){
+    /* dummy params for Neutron Subnet */
+    public NeutronSubnet defaultSubnetObject() {
         NeutronSubnet subnet = new NeutronSubnet();
         subnet.setNetworkUUID("6b9570f2-17b1-4fc399ec-1b7f7778a29b");
         subnet.setSubnetUUID("7b9570f2-17b1-4fc399ec-1b7f7778a29b");
@@ -73,62 +71,58 @@ public class SubnetHandlerTest {
     public void testCanCreateSubnetVirtualNetworkNull() throws IOException {
         Activator.apiConnector = mockedApiConnector;
         NeutronSubnet neutronSubnet = defaultSubnetObject();
-        when(mockedApiConnector.findById(VirtualNetwork.class,neutronSubnet.getNetworkUUID())).thenReturn(null);
+        when(mockedApiConnector.findById(VirtualNetwork.class, neutronSubnet.getNetworkUUID())).thenReturn(null);
         assertEquals(HttpURLConnection.HTTP_FORBIDDEN, subnetHandler.canCreateSubnet(neutronSubnet));
     }
-
 
     /* Test method to check if subnet can be created with IpamRefs null */
     @Test
     public void testCanCreateSubnet() throws IOException {
         Activator.apiConnector = mockedApiConnector;
         NeutronSubnet neutronSubnet = defaultSubnetObject();
-        when(mockedApiConnector.findById(VirtualNetwork.class,neutronSubnet.getNetworkUUID())).thenReturn(mockedVirtualNetwork);
+        when(mockedApiConnector.findById(VirtualNetwork.class, neutronSubnet.getNetworkUUID())).thenReturn(mockedVirtualNetwork);
         when(mockedVirtualNetwork.getNetworkIpam()).thenReturn(null);
         when(mockedApiConnector.update(mockedVirtualNetwork)).thenReturn(true);
         assertEquals(HttpURLConnection.HTTP_OK, subnetHandler.canCreateSubnet(neutronSubnet));
     }
 
-
-    /* Test method to check if subnet creation returns Internal Server Error  */
+    /* Test method to check if subnet creation returns Internal Server Error */
     @Test
     public void testCanCreateSubnetException() throws IOException {
         Activator.apiConnector = mockedApiConnector;
         NeutronSubnet neutronSubnet = defaultSubnetObject();
-        when(mockedApiConnector.findById(VirtualNetwork.class,neutronSubnet.getNetworkUUID())).thenReturn(mockedVirtualNetwork);
+        when(mockedApiConnector.findById(VirtualNetwork.class, neutronSubnet.getNetworkUUID())).thenReturn(mockedVirtualNetwork);
         when(mockedVirtualNetwork.getNetworkIpam()).thenReturn(null);
         when(mockedApiConnector.update(mockedVirtualNetwork)).thenReturn(false);
         assertEquals(HttpURLConnection.HTTP_INTERNAL_ERROR, subnetHandler.canCreateSubnet(neutronSubnet));
     }
 
-
-    /* Test method to check if subnet already exists  */
+    /* Test method to check if subnet already exists */
     @Test
     public void testCanCreateSubnetExists() throws IOException {
         Activator.apiConnector = mockedApiConnector;
         NeutronSubnet neutronSubnet = defaultSubnetObject();
-        when(mockedApiConnector.findById(VirtualNetwork.class,neutronSubnet.getNetworkUUID())).thenReturn(mockedVirtualNetwork);
+        when(mockedApiConnector.findById(VirtualNetwork.class, neutronSubnet.getNetworkUUID())).thenReturn(mockedVirtualNetwork);
         VnSubnetsType vnSubnetType = new VnSubnetsType();
-        ObjectReference <VnSubnetsType> ref=new ObjectReference<>();
-        List<ObjectReference <VnSubnetsType>> ipamRefs = new ArrayList<ObjectReference <VnSubnetsType>>();
-        List<VnSubnetsType.IpamSubnetType> subnets=new ArrayList<VnSubnetsType.IpamSubnetType>();
+        ObjectReference<VnSubnetsType> ref = new ObjectReference<>();
+        List<ObjectReference<VnSubnetsType>> ipamRefs = new ArrayList<ObjectReference<VnSubnetsType>>();
+        List<VnSubnetsType.IpamSubnetType> subnets = new ArrayList<VnSubnetsType.IpamSubnetType>();
         VnSubnetsType.IpamSubnetType subnetType = new VnSubnetsType.IpamSubnetType();
         SubnetType type = new SubnetType();
-        List<String> temp=new ArrayList<String>();
-        for(int i=0;i<1;i++) {
+        List<String> temp = new ArrayList<String>();
+        for (int i = 0; i < 1; i++) {
             subnetType.setSubnet(type);
             subnetType.getSubnet().setIpPrefix("10.0.0.1");
             subnetType.getSubnet().setIpPrefixLen(24);
             subnets.add(subnetType);
             vnSubnetType.addIpamSubnets(subnetType);
-            ref.setReference(temp,vnSubnetType,"","");
+            ref.setReference(temp, vnSubnetType, "", "");
             ipamRefs.add(ref);
-            }
+        }
         when(mockedVirtualNetwork.getNetworkIpam()).thenReturn(ipamRefs);
         assertTrue(subnetType.getSubnet().getIpPrefix().matches("10.0.0.1"));
         assertEquals(HttpURLConnection.HTTP_FORBIDDEN, subnetHandler.canCreateSubnet(neutronSubnet));
-        }
-
+    }
 
     /* Test method to check if subnet already exists, with null CIDR */
     @Test
@@ -137,30 +131,29 @@ public class SubnetHandlerTest {
         NeutronSubnet neutronSubnet = new NeutronSubnet();
         neutronSubnet.setNetworkUUID("6b9570f2-17b1-4fc399ec-1b7f7778a29b");
         neutronSubnet.setSubnetUUID("7b9570f2-17b1-4fc399ec-1b7f7778a29b");
-        when(mockedApiConnector.findById(VirtualNetwork.class,neutronSubnet.getNetworkUUID())).thenReturn(mockedVirtualNetwork);
+        when(mockedApiConnector.findById(VirtualNetwork.class, neutronSubnet.getNetworkUUID())).thenReturn(mockedVirtualNetwork);
         VnSubnetsType vnSubnetType = new VnSubnetsType();
-        ObjectReference <VnSubnetsType> ref=new ObjectReference<>();
-        List<ObjectReference <VnSubnetsType>> ipamRefs = new ArrayList<ObjectReference <VnSubnetsType>>();
-        List<VnSubnetsType.IpamSubnetType> subnets=new ArrayList<VnSubnetsType.IpamSubnetType>();
+        ObjectReference<VnSubnetsType> ref = new ObjectReference<>();
+        List<ObjectReference<VnSubnetsType>> ipamRefs = new ArrayList<ObjectReference<VnSubnetsType>>();
+        List<VnSubnetsType.IpamSubnetType> subnets = new ArrayList<VnSubnetsType.IpamSubnetType>();
         VnSubnetsType.IpamSubnetType subnetType = new VnSubnetsType.IpamSubnetType();
         SubnetType type = new SubnetType();
-        List<String> temp=new ArrayList<String>();
-        for(int i=0;i<1;i++) {
+        List<String> temp = new ArrayList<String>();
+        for (int i = 0; i < 1; i++) {
             subnetType.setSubnet(type);
             subnetType.getSubnet().setIpPrefix("10.0.0.1");
             subnetType.getSubnet().setIpPrefixLen(24);
             subnets.add(subnetType);
             vnSubnetType.addIpamSubnets(subnetType);
-            ref.setReference(temp,vnSubnetType,"","");
+            ref.setReference(temp, vnSubnetType, "", "");
             ipamRefs.add(ref);
-            }
+        }
         when(mockedVirtualNetwork.getNetworkIpam()).thenReturn(ipamRefs);
         assertTrue(subnetType.getSubnet().getIpPrefix().matches("10.0.0.1"));
         assertEquals(HttpURLConnection.HTTP_BAD_REQUEST, subnetHandler.canCreateSubnet(neutronSubnet));
-        }
+    }
 
-
-    /* Test method to check ipPrefix  */
+    /* Test method to check ipPrefix */
     @Test
     public void testGetIpPrefix() {
         SubnetHandler.apiConnector = mockedApiConnector;
@@ -170,9 +163,8 @@ public class SubnetHandlerTest {
         assertArrayEquals(ipPrefix, subnetHandler.getIpPrefix(neutronSubnet));
     }
 
-
-    /* Test method to check if ipPrefix is null  */
-    @Test(expected=NullPointerException.class)
+    /* Test method to check if ipPrefix is null */
+    @Test(expected = NullPointerException.class)
     public void testGetIpPrefixNull() {
         SubnetHandler.apiConnector = mockedApiConnector;
         NeutronSubnet neutronSubnet = new NeutronSubnet();
@@ -181,9 +173,8 @@ public class SubnetHandlerTest {
         subnetHandler.getIpPrefix(neutronSubnet);
     }
 
-
-    /* Test method to check if ipPrefix is valid  */
-    @Test(expected=IllegalArgumentException.class)
+    /* Test method to check if ipPrefix is valid */
+    @Test(expected = IllegalArgumentException.class)
     public void testGetIpPrefixInvalid() {
         SubnetHandler.apiConnector = mockedApiConnector;
         NeutronSubnet neutronSubnet = new NeutronSubnet();
@@ -193,13 +184,12 @@ public class SubnetHandlerTest {
         subnetHandler.getIpPrefix(neutronSubnet);
     }
 
-
-    /* Test method to check if network is available  */
+    /* Test method to check if network is available */
     @Test
     public void testGetNetwork() throws IOException {
         SubnetHandler.apiConnector = mockedApiConnector;
         NeutronSubnet neutronSubnet = defaultSubnetObject();
-        when(mockedApiConnector.findById(VirtualNetwork.class,neutronSubnet.getNetworkUUID())).thenReturn(mockedVirtualNetwork);
+        when(mockedApiConnector.findById(VirtualNetwork.class, neutronSubnet.getNetworkUUID())).thenReturn(mockedVirtualNetwork);
         assertNotNull(subnetHandler.getNetwork(neutronSubnet));
     }
 }
